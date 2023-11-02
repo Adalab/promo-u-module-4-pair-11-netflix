41eaaa3aea2e44e3d6307a068cdc2dcaec6c3682
crisrodriguezgar <rodriguezgar.cris@gmail.com>
2023-11-02 11:38:33 +0100

    added connection database

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+const mysql = require ("mysql2/promise")
+
 const express = require('express');
 const cors = require('cors');
 
@@ -10,4 +12,40 @@ server.use(express.json());
 const serverPort = 4000;
 server.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
+});
+
+//conexion a la base de datos
+async function getConnection() {
+  //crear y configurar la conexion
+  const connection = await mysql.createConnection({
+    host: 'Localhost',
+    user: 'root',
+    password: 'Sonrie2132',
+    database: 'netflix',
+  });
+  connection.connect();
+  return connection;
+}
+
+//iniciar el servidor
+const port = 4000;
+server.listen(port, () => {
+  console.log(`Servidor iniciado en http://localhost:${port}`);
+});
+
+
+//endpoint para todas las películas
+server.get('/movies', async (req, res) => {
+  //1. Obtener los datos de la base de datos
+  const conn = await getConnection();
+  //2. Consulta que quiero a la bd: obtener todas las alumnas
+  const queryMovies = 'SELECT * FROM movies';
+  //3. Ejecutar la consulta
+  const [results, fields] = await conn.query(queryMovies);
+  console.log(fields);
+  console.log(results);
+  //4.Cerrar la conexión
+  conn.end();
+
+  res.json(results);
 });
