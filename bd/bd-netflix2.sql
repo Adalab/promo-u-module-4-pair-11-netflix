c0de413079fe3c5eecd06717ebc9b18f5033af37
virchaca <virginia.alvarez82@gmail.com>
2023-11-03 17:12:31 +0100

    updating BD

--- bd/bd-netflix2.sql
+++ bd/bd-netflix2.sql
@@ -5,4 +5,16 @@ ALTER TABLE actors ADD imagen varchar(60);
 
 
 ALTER TABLE rel_movies_users ADD score INT;
-SELECT * FROM rel_movies_users
+SELECT * FROM rel_movies_users;
+
+SELECT * FROM movies;
+INSERT INTO movies (title, genre, image, category, year)
+VALUE ('gambita de dama', 'drama', 'https://localhost:4000/gambita-de-dama.jpg', 'top 10', 2020);
+
+INSERT INTO movies (title, genre, image, category, year)
+VALUE ('friends', 'comedia', 'https://localhost:4000/friends.jpg', 'top 10', 1994)
+
+UPDATE `newnetflix`.`movies` SET `image` = 'http://localhost:4000/gambita-de-dama.jpg' WHERE (`idMovies` = '4');
+UPDATE `newnetflix`.`movies` SET `image` = 'http://localhost:4000/friends.jpg' WHERE (`idMovies` = '5');
+
+SELECT genre FROM movies
